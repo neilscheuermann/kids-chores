2720967bc5571b1bfff4760fa3a380a4d01fc955
Neil Scheuermann <neilscheuermann@gmail.com>
2020-06-30 22:50:07 -0600

    change the server host to 4005

--- client/src/util/apollo.js
+++ client/src/util/apollo.js
@@ -2,7 +2,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 
-const HTTP_URI = "http://localhost:4000";
+const HTTP_URI = "http://localhost:4005";
 export const createClient = () => {
   return new ApolloClient({
     // we will change this later when seeting up the socket
--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :kids_chores, KidsChores.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :kids_chores, KidsChoresWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4005],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
